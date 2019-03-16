bc92f214613af0728e3ed6a3ce776951093868d3
Esten Rye <esten.rye@ryezone.com>
2019-03-16 10:47:28 -0500

    Fix broken cleanup logic.

--- build/scripts/cleanup.sh
+++ build/scripts/cleanup.sh
@@ -10,6 +10,7 @@
 
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
+rm -rf /opt/ansible
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
