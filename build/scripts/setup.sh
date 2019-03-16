98719c9e28f21cb45869c3fc9c359ef29d17324f
Esten Rye <esten@ryezone.net>
2019-03-16 02:23:04 -0500

    Add additional steps to create and grant permissions

--- build/scripts/setup.sh
+++ build/scripts/setup.sh
@@ -3,3 +3,6 @@
 # Add vagrant user to sudoers.
 echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+mkdir -p /opt/ansible
+chown vagrant /opt/ansible
+chgrp vagrant /opt/ansible
