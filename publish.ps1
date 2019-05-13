acb409248e7c9345f0a903fc52eaa3a07069ac28
Esten Rye <esten@ryezone.net>
2019-05-13 00:21:10 -0500

    Add debugging statements. Clean up publish.ps1.

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 param(
   $vagrant_box_name = $env:VAGRANT_BOX_NAME,
   $vagrant_box_version = $(if([string]::IsNullOrWhiteSpace($env:VAGRANT_BOX_VERSION))
@@ -23,13 +24,50 @@ param(
   $jq_version = '1.6'
 )
 
+Write-Debug $(@{
+  vagrant_box_name = $vagrant_box_name
+  vagrant_box_version = $vagrant_box_version
+  vagrant_provider_type = $vagrant_provider_type
+  vagrant_cloud_token = $vagrant_cloud_token
+  vagrant_cloud_username = $vagrant_cloud_username
+  publish = $publish
+  upload = $upload
+  release = $release
+  vagrant_api = $vagrant_api
+  s3_endpoint = $s3_endpoint
+  s3_bucket = $s3_bucket
+  s3_profile = $s3_profile
+  jq_version = $jq_version
+  powershell_version = $PSVersionTable.PSVersion.ToString()
+} | ConvertTo-Json)
+
+if([string]::IsNullOrWhiteSpace($vagrant_box_name))
+{ 
+  Write-Error "vagrant_box_name cannot be null or whitespace."
+  return -1
+}
+
+if ([string]::IsNullOrWhiteSpace($vagrant_cloud_token))
+{
+  Write-Error "vagrant_cloud_token cannot be null or whitespace"
+  return -1
+}
+
+if ([string]::IsNullOrWhiteSpace($env:VAGRANT_CLOUD_USERNAME))
+{
+  Write-Error "vagrant_cloud_username cannot be null or whitespace."
+  return -1
+}
+
+if ($PSVersionTable.PSVersion.Major -lt 6)
+{
+  Write-Error "Powershell version must be 6 or higher."
+  return -1
+}
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $dir = Resolve-Path -Path "${PSScriptRoot}/build"
-$python_packages = $(pip show awscli | Select-String -Pattern 'Location:*')
-$python_packages = ${python_packages}.ToString().Replace('Location: ', '')
-$aws_cli = Resolve-Path -Path "${python_packages}/awscli"
-
 Push-Location ${dir}
 
 $manifest = Get-Content manifest.json -Raw | ConvertFrom-Json
@@ -41,7 +79,11 @@ $s3_box_path = "${s3_bucket}/${vagrant_box_name}/${vagrant_provider_type}-${vagr
 
 if (${upload} -or ${all})
 {
-  python ${aws_cli} s3 cp `
+  Write-Debug $(@{
+    boxfile = "images/${boxfile}"
+    s3Path = "s3://${s3_box_path}"
+  } | ConvertTo-Json)
+  aws s3 cp `
     "images/${boxfile}" `
     "s3://${s3_box_path}" `
     --endpoint-url=${s3_endpoint} `
@@ -52,15 +94,25 @@ $box_api = "${vagrant_api}/box/${vagrant_cloud_username}/${vagrant_box_name}"
 $token = ConvertTo-SecureString -String ${vagrant_cloud_token} -AsPlainText -Force
 if (${publish} -or ${all})
 {
+  Write-Debug $(@{
+    method = 'GET'
+    uri = $box_api
+  } | ConvertTo-Json)
   $getBox = Invoke-WebRequest `
               -UseBasicParsing `
-              -Uri "${vagrant_api}/box/${vagrant_cloud_username}/${vagrant_box_name}" `
+              -Uri $box_api `
               -ContentType 'application/json' `
               -Headers ${header} `
               -Method Get `
               -Authentication Bearer `
               -Token ${token} `
               -ErrorAction SilentlyContinue
+
+  Write-Debug $(@{
+    method = 'GET'
+    uri = $box_api
+    statusCode = ${getBox}.StatusCode
+  } | ConvertTo-Json)
   
   if (${getBox}.StatusCode -ne 200)
   {
@@ -72,6 +124,11 @@ if (${publish} -or ${all})
       }
     } | ConvertTo-Json
 
+    Write-Debug $(@{
+      uri = "${vagrant_api}/boxes"
+      method = 'POST'
+      body = $body
+    } | ConvertTo-Json)
     Invoke-RestMethod `
       -UseBasicParsing `
       -Uri "${vagrant_api}/boxes" `
@@ -83,6 +140,11 @@ if (${publish} -or ${all})
       -Body ${body}
   }  
 
+  Write-Debug $(@{
+    method = 'GET'
+    uri = "${box_api}/version/${vagrant_box_version}"
+  } | ConvertTo-Json)
+
   $getVersion = Invoke-WebRequest `
     -UseBasicParsing `
     -Uri "${box_api}/version/${vagrant_box_version}" `
@@ -93,14 +155,26 @@ if (${publish} -or ${all})
     -Token ${token} `
     -Body ${body}
 
-  $body = @{
-    version = @{
-      version = ${vagrant_box_version}
-    }
-  } | ConvertTo-Json
+  Write-Debug $(@{
+    method = 'GET'
+    uri = "${box_api}/version/${vagrant_box_version}"
+    statusCode = ${getVersion}.StatusCode
+  } | ConvertTo-Json)
 
   if (${getVersion}.StatusCode -ne 200)
   {
+    $body = @{
+      version = @{
+        version = ${vagrant_box_version}
+      }
+    } | ConvertTo-Json
+  
+    Write-Debug $(@{
+      method = 'POST'
+      uri = "${box_api}/versions"
+      body = $body
+    } | ConvertTo-Json)
+
     Invoke-RestMethod `
       -UseBasicParsing `
       -Uri "${box_api}/versions" `
@@ -131,6 +205,12 @@ if (${publish} -or ${all})
     $uri = "${box_api}/version/${vagrant_box_version}/provider/${vagrant_provider_type}"
   }
 
+  Write-Debug $(@{
+    method = $method
+    uri = $uri
+    body = $body
+  } | ConvertTo-Json)
+
   Invoke-RestMethod `
     -UseBasicParsing `
     -Uri $uri `
@@ -144,6 +224,11 @@ if (${publish} -or ${all})
 
 if (${release} -or ${all})
 {
+  Write-Debug $(@{
+    method = 'PUT'
+    uri = "${box_api}/version/${vagrant_box_version}/release"
+  } | ConvertTo-Json)
+
   Invoke-RestMethod `
     -UseBasicParsing `
     -Uri "${box_api}/version/${vagrant_box_version}/release" `
