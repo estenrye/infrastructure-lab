96ce2841fb561161e1170d725858ac28b605b3e4
Esten Rye <esten.rye@ryezone.com>
2019-05-22 03:25:27 -0500

    fix script errors.

--- .demo/demo.ps1
+++ .demo/demo.ps1
@@ -1,6 +1,6 @@
 Push-Location ${PSScriptRoot}/..
 
-vagrant ssh manager -c 'docker network create -d overlay public'
-vagrant ssh manager -c 'docker network create -d overlay private'
-scp stack.yml jdoe@10.100.10.3:~/stack.yml
-vagrant ssh manager -c 'docker stack deploy -c ~/.stack.yml demo'
+scp ./.demo/stack.yml jdoe@10.100.10.3:/tmp/stack.yml
+ssh jdoe@10.100.10.3 -C 'chmod 644 /tmp/stack.yml' 
+vagrant ssh manager -c 'docker network create -d overlay public; docker network create -d overlay private; docker stack deploy -c /tmp/stack.yml demo'
+Pop-Location
